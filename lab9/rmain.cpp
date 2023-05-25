8c36ecf36f0ca1cf9b037bc73886706e9c08b1a9
larry487 <119911822+larry487@users.noreply.github.com>
2023-05-25 14:36:24 +0800

    Create rmain.cpp

---
+++ lab9/rmain.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class ReplaceMyString {
+    public:
+    ReplaceMyString() {}
+    void replaceString(string a, string b) {
+        ifstream in("main.cpp");
+        ofstream out("rmain.cpp");
+        int i, j, alen = a.length(), blen = b.length();
+        char c;
+        
+        do{
+            in.get(c);
+            if (in.eof()) break;
+            if (c == a[0]) {
+                for (i=1; i<a.length(); i++) {
+                    in>>c;
+                    if (c != a[i]) {
+                        break;
+                    }
+                    if (i == a.length()-1) {
+                        for (j=0; j<b.length(); j++) {
+                            out << b[j];
+                        }
+                    }
+                }
+            } else 
+                out<<c;
+        }while(!in.eof());
+        
+        in.close();
+        out.close();
+    }
+};
+
+int main() {
+    ReplaceMyString my;
+    my.replaceString("IU is best", "IU is best is best");
+    return 0;
+}
