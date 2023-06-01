16e8bb5c2cdb700a3f9e02909927a37659d40125
larry487 <119911822+larry487@users.noreply.github.com>
2023-06-01 14:52:55 +0800

    main.cpp

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -1,53 +1,44 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-
+#include <fstream>
 using namespace std;
 
 class myString
 {
-private:
-    string my_str;
-public:
+    private:
+    string m_str;
+    public:
     myString(string s){
-        my_str = s;
+        m_str=s;
     }
 };
 
 class ReadClass
 {
-public:
-   void showClass(){
-       ifstream input;
-       input.open("main.cpp");
-       string line;
-       int count = 0;
-       while (getline(input, line)) {
-           if (line.find("class ") != string::npos && (int)line.find("class ") == 0) {
-               count++;
-           }
-       }
-       input.close();
-       cout<<count<<" class in main.cpp"<<endl;
-       
-       input.open("main.cpp");
-       
-       string out;
-       
-       while(getline(input, line)){
-           
-           if(line.find("class ") != string::npos && (int)line.find("class ") == 0){
-               size_t startpos = line.find("class ") + 6;
-               size_t endpos = line.find("{");
-               out = line.substr(startpos, endpos);
-               cout<<"class "<<out<<endl;
-           }
-       }
-       input.close();
-   }
+    public:
+    void showClass(){
+        int n=0;
+        ifstream in;
+        string line,str("class");
+        in.open("main.cpp");
+        if(in.fail()){
+            cout<<"Erro to open file."<<endl;
+        }
+        while (getline(in, line)) {
+            if (line.find(str) != string::npos){
+                const char* characters = line.c_str();
+                if(characters[0]=='c'){
+                    cout << characters << endl;
+                    n++;
+                }
+            }
+        }
+        cout<<n<<" class in main.cpp"<<endl;
+    }
 };
 
-int main() {
+int main()
+{
     ReadClass rfile;
     rfile.showClass();
     return 0;
